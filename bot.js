2c39f69909c270d26a5f8a66e19899a41fe58562
Sailesh Patel <saileshapatel@gmail.com>
2015-10-31 22:23:00 +0000

    made bot random

--- bot.js
+++ bot.js
@@ -9,6 +9,16 @@ var Bot = new TwitterBot({
   });
 
 
-var witches = Bot.tweet("Why don't witches like to ride their brooms when they're angry? \n They're afraid of flying off the handle.");
-var dayscare = Bot.tweet("Where do baby ghosts go during the day? \n Dayscare centers.");
-var frankenstein = Bot.tweet("Who did Frankenstein take to the prom? \n His ghoul friend.");
+var witches = "Q: Why don't witches like to ride their brooms when they're angry? \n A: They're afraid of flying off the handle."
+var dayscare = "Q: Where do baby ghosts go during the day? \n A: Dayscare centers."
+var frankenstein = "Q: Who did Frankenstein take to the prom? \n A: His ghoul friend."
+var bone = "Q: What do skeletons say before they begin dining? \n A: Bone appetite!"
+var skeleton = "Q: Who won the skeleton beauty contest? \n A: No body."
+var romeo = "Q: What's a monster's favorite play? \n A: Romeo and Ghouliet."
+var dracula = "Q: How can you tell when you're in bed with Count Dracula? \n A: He has a big D on his pajamas."
+
+var jokes = [witches, dayscare, frankenstein, bone, skeleton, romeo, dracula];
+
+var random = jokes[Math.floor(Math.random()*jokes.length)];
+
+Bot.tweet(random); 
