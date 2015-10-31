be265dd55721962ff9bffa6bda22f27625383db0
Sailesh Patel <saileshapatel@gmail.com>
2015-10-31 20:55:26 +0000

    got rid of twitter credentials

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@ var TwitterBot = require("node-twitterbot").TwitterBot;
 
 // Include your access information below
 var Bot = new TwitterBot({
-  "consumer_secret": "Mi5AsPenHJVLZ0bGckYjlvzLE07AZ9nLcKM6rni4JO44UjdVn9",
-  "consumer_key": "TMKLbVlzS57xwNJnQSisfh4nQ",
-  "access_token": "4082975487-ygJ5i8pFLdP3MbND651LQMvRcEatGyTEXNq41ZI",
-  "access_token_secret": "AP0n4xk8UNJppIvn7cXiIwucC2EVCBRRKH1YCItREPrYR"
-});
+  "consumer_secret": "consumer_secret",
+    "consumer_key": "consumer_key",
+    "access_token": "access_token",
+    "access_token_secret": "access_token_secret"
+  });
 
 
 var witches = Bot.tweet("Why don't witches like to ride their brooms when they're angry? \n They're afraid of flying off the handle.");
