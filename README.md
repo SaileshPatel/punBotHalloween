090dbc2fc574a79421694bad43a2d180effeb15a
Sailesh Patel <saileshapatel@gmail.com>
2015-11-01 01:21:26 +0000

    initial commit

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 A quick Twitter bot, which will hopefully in the future respond to tweets to '@halloweenPuns'
 
+## 
+
 ## Things to do
 - Make a listening stream to tweets can be detected
 - Generate an automatic response to said tweets 
 
-## How to run
+## Requirements
 In order to run this bot, you will need: 
 - Node.js
 - npm
 - node-twitterbot
-
--- bot.js
+++ bot.js
@@ -2,24 +2,28 @@ var TwitterBot = require("node-twitterbot").TwitterBot;
 
 // Include your access information below
 var Bot = new TwitterBot({
-  "consumer_secret": "consumer_secret",
+  	"consumer_secret": "consumer_secret",
     "consumer_key": "consumer_key",
     "access_token": "access_token",
     "access_token_secret": "access_token_secret"
   });
 
-var streamAction = new TwitterBotStreamAction(null, Bot)
-
-var witches = "Q: Why don't witches like to ride their brooms when they're angry? \n A: They're afraid of flying off the handle."
-var dayscare = "Q: Where do baby ghosts go during the day? \n A: Dayscare centers."
-var frankenstein = "Q: Who did Frankenstein take to the prom? \n A: His ghoul friend."
-var bone = "Q: What do skeletons say before they begin dining? \n A: Bone appetite!"
-var skeleton = "Q: Who won the skeleton beauty contest? \n A: No body."
-var romeo = "Q: What's a monster's favorite play? \n A: Romeo and Ghouliet."
-var dracula = "Q: How can you tell when you're in bed with Count Dracula? \n A: He has a big D on his pajamas."
+var witches = "Q: Why don't witches like to ride their brooms when they're angry? \nA: They're afraid of flying off the handle."
+var dayscare = "Q: Where do baby ghosts go during the day? \nA: Dayscare centers."
+var frankenstein = "Q: Who did Frankenstein take to the prom? \nA: His ghoul friend."
+var bone = "Q: What do skeletons say before they begin dining? \nA: Bone appetite!"
+var skeleton = "Q: Who won the skeleton beauty contest? \nA: No body."
+var romeo = "Q: What's a monster's favorite play? \nA: Romeo and Ghouliet."
+var dracula = "Q: How can you tell when you're in bed with Count Dracula? \nA: He has a big D on his pajamas."
 
 var jokes = [witches, dayscare, frankenstein, bone, skeleton, romeo, dracula];
 
 var random = jokes[Math.floor(Math.random()*jokes.length)];
 
-Bot.tweet(random); 
+// Create an action called 'tweet' associated with some function
+Bot.addAction("tweet", function(twitter, action, tweet) {
+  // Within the action 'tweet', tell the Bot to create a tweet
+  Bot.tweet(random);
+});
+
+Bot.now("tweet"); 
